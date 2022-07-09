e0756858ff3b261dc0e37d9fdbd0389aa9b5ce61
newSmile726 <1432072169@qq.com>
2022-07-09 20:38:16 +0800

    完成练习

---
+++ src/views/fenlei.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <h3>吃喝</h3>
+    <h3>玩乐</h3>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {};
+  },
+  mounted() {},
+  computed: {},
+  methods: {},
+};
+</script>
+<style scoped></style>
