dc7151516793e9563bcb034713c4083b09e72cc2
baibaixy <2942583916@qq.com>
2022-07-09 20:02:25 +0800

    wnagye

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,29 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <!-- <router-link to="/shouye">首页</router-link> -->
+    <span @click='btn("/shouye")'>首页</span>
+    <router-link to="/fenlei">分类</router-link>
+    <router-link to="/dingdan">订单</router-link>
+    <router-link to="/wode">我的</router-link>
+    <router-view></router-view>
   </div>
 </template>
-
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  data() {
+    return {};
+  },
+  mounted() {},
+  computed: {},
+  methods: {
+    btn(targetPath) {
+      // 方式1: path跳转
+      this.$router.push({
+        path: targetPath,
+       
+      });
+    },
+  },
+};
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<style scoped></style>
